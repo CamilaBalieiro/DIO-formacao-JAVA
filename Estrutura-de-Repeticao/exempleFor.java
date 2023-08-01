241a89ad87b2a35f3926b7edd8a4f071d706e090
CamilaBalieiro <joy.rodrigues.cout@gmail.com>
2023-08-01 20:00:15 -0300

    main

---
+++ Estrutura-de-Repeticao/exempleFor.java
@@ -0,0 +1,10 @@
+public class exempleFor{
+   
+    public static void main (String[] args){
+        for(int carneirinho = 1; carneirinho<=20;carneirinho++){
+            System.out.println("Contando Carneirinhos:" + carneirinho);
+        }
+        System.out.println("Joaozinho Dormiu zzzzzz");
+    }
+
+}
